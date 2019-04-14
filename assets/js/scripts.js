bec496de8f0e5be2184d25370976b04db728553a
Branko Sojic <bsojic>
2019-04-14 15:41:45 +0200

    commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".nav .icon").click(function(){
+    $("#links").toggle();
+  });
+  $(".nav .search-icon").click(function(){
+    $("#search-input").toggle();
+  });
+});
---
+++ assets/js/swipper.js
@@ -0,0 +1,13 @@
+ var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        slidesPerView: 'auto',
+        paginationClickable: true,
+        spaceBetween: 0,
+         navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+    });
